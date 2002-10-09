9a1c66e4ce7c728ae081a7f285c15e1b6678912d
swquek-oss <swquek-oss@251d0590-4201-4cf1-90de-194747b24ca1>
2002-10-09 18:48:37 +0000

    ticket:2323: branching for release 2.2.1

--- icu4j/branches/branch-release-2-2-1/src/com/ibm/icu/dev/test/translit/UnicodeSetTest.java
+++ icu4j/branches/branch-release-2-2-1/src/com/ibm/icu/dev/test/translit/UnicodeSetTest.java
@@ -5,8 +5,8 @@
  *******************************************************************************
  *
  * $Source: /xsrl/Nsvn/icu/icu4j/src/com/ibm/icu/dev/test/translit/UnicodeSetTest.java,v $ 
- * $Date: 2002/06/06 22:39:31 $ 
- * $Revision: 1.30 $
+ * $Date: 2002/10/09 18:48:37 $ 
+ * $Revision: 1.30.2.1 $
  *
  *****************************************************************************************
  */
@@ -787,6 +787,31 @@ public class UnicodeSetTest extends TestFmwk {
             }
         }
     }
+
+    /** 
+     * Make sure each script name and abbreviated name can be used 
+     * to construct a UnicodeSet. 
+     */ 
+    public void TestScriptNames() { 
+        for (int i=0; i<UScript.CODE_LIMIT; ++i) { 
+            for (int j=0; j<2; ++j) { 
+                String pat = ""; 
+                try { 
+                    String name = 
+                        (j==0) ? UScript.getName(i) : UScript.getShortName(i); 
+                    pat = "[:" + name + ":]"; 
+                    UnicodeSet set = new UnicodeSet(pat); 
+                    logln("Ok: " + pat); 
+                } catch (IllegalArgumentException e) { 
+                    if (pat.length() == 0) { 
+                        errln("FAIL (in UScript): No name for script " + i); 
+                    } else { 
+                        errln("FAIL: Couldn't create " + pat); 
+                    } 
+                } 
+            } 
+        } 
+    } 
     
     void _testComplement(int a) {
         UnicodeSet x = bitsToSet(a);
