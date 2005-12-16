d993832ec51b9c26068c5f1996ea884ee5ae0a70
nobody <nobody@251d0590-4201-4cf1-90de-194747b24ca1>
2005-12-16 20:02:53 +0000

    This commit was manufactured by cvs2svn to create tag 'release-3-4-2-d02'.

--- src/com/ibm/icu/text/DateFormatSymbols.java
+++ src/com/ibm/icu/text/DateFormatSymbols.java
@@ -7,6 +7,7 @@
 
 package com.ibm.icu.text;
 
+import com.ibm.icu.impl.ICULocaleData;
 import com.ibm.icu.impl.ICUResourceBundle;
 import com.ibm.icu.impl.CalendarData;
 import com.ibm.icu.impl.Utility;
@@ -1547,9 +1548,9 @@ public class DateFormatSymbols implements Serializable, Cloneable {
 
         String bundleName = className + "Symbols";
 
-        UResourceBundle result = null;
+        ResourceBundle result = null;
         try {
-            result = UResourceBundle.getBundleInstance(bundleName, locale);
+            result = ICULocaleData.getResourceBundle(bundleName, locale);
         }
         catch (MissingResourceException e) {
             ///CLOVER:OFF
