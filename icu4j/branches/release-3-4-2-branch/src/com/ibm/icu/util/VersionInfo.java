21fb0f169735c8f24151f8f0f8fc49a6057ec034
srl <srl@251d0590-4201-4cf1-90de-194747b24ca1>
2006-02-15 02:28:27 +0000

    ticket:5058: 3.4.3

--- icu4j/branches/release-3-4-2-branch/src/com/ibm/icu/util/VersionInfo.java
+++ icu4j/branches/release-3-4-2-branch/src/com/ibm/icu/util/VersionInfo.java
@@ -1,6 +1,6 @@
 /*
  *******************************************************************************
- * Copyright (C) 1996-2005, International Business Machines Corporation and    *
+ * Copyright (C) 1996-2006, International Business Machines Corporation and    *
  * others. All Rights Reserved.                                                *
  *******************************************************************************
  */
@@ -441,7 +441,7 @@ public final class VersionInfo
         UNICODE_4_0   = getInstance(4, 0, 0, 0);
         UNICODE_4_0_1 = getInstance(4, 0, 1, 0);
         UNICODE_4_1   = getInstance(4, 1, 0, 0);
-        ICU_VERSION   = getInstance(3, 4, 2, 0);
+        ICU_VERSION   = getInstance(3, 4, 3, 0);
         UCOL_RUNTIME_VERSION = getInstance(6);
         UCOL_BUILDER_VERSION = getInstance(7);
         UCOL_TAILORINGS_VERSION = getInstance(1);
