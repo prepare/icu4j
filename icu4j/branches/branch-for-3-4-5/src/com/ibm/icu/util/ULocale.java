60a53cb731aaa6ca7715f5b5e0fd7d611cfa05c8
yoshito <yoshito@251d0590-4201-4cf1-90de-194747b24ca1>
2006-08-02 03:24:48 +0000

    ticket:5289: fallback to "root" instead of "" - same fix with JB5194 for 3.4.5 release

--- icu4j/branches/branch-for-3-4-5/src/com/ibm/icu/util/ULocale.java
+++ icu4j/branches/branch-for-3-4-5/src/com/ibm/icu/util/ULocale.java
@@ -229,7 +229,7 @@ public final class ULocale implements Serializable {
      * The root ULocale.
      * @stable ICU 2.8
      */ 
-    public static final ULocale ROOT = new ULocale(EMPTY_STRING, EMPTY_LOCALE);
+    public static final ULocale ROOT = new ULocale("root", EMPTY_LOCALE);
     
     private static final HashMap CACHE = new HashMap(20);
     static {
@@ -747,6 +747,9 @@ public final class ULocale implements Serializable {
     public static ULocale forLocale(Locale loc) {
         if (loc == null) {
             return null;
+        }
+        if (loc.toString().length() == 0) {
+            return ROOT;
         }
         ULocale result = (ULocale)CACHE.get(loc);
         if (result == null && defaultULocale != null && loc == defaultULocale.locale) {
