71b2aa98c528a1e0ee92f3c042d782b31efb2718
shaopengjia <shaopengjia@251d0590-4201-4cf1-90de-194747b24ca1>
2008-11-19 22:49:49 +0000

    ticket:6604: testing commit changes to my branch

---
+++ ensel/src/com/ibm/icu/impl/PropsVectors.java
@@ -0,0 +1,8 @@
+/*
+******************************************************************************
+* Copyright (C) 1996-2008, International Business Machines Corporation and   *
+* others. All Rights Reserved.                                               *
+******************************************************************************
+*/
+
+package com.ibm.icu.impl;
