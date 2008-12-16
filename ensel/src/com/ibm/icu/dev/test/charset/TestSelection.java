772423d27402ba241db51a47d89d7dcafbe27826
shaopengjia <shaopengjia@251d0590-4201-4cf1-90de-194747b24ca1>
2008-12-16 11:25:27 +0000

    ticket:6604: Porting CharsetSelector

--- ensel/src/com/ibm/icu/dev/test/charset/TestSelection.java
+++ ensel/src/com/ibm/icu/dev/test/charset/TestSelection.java
@@ -19,20 +19,86 @@
 
 package com.ibm.icu.dev.test.charset;
 
+import java.nio.charset.Charset;
+import java.util.List;
 import java.util.Vector;
 
 import com.ibm.icu.charset.CharsetICU;
 import com.ibm.icu.charset.CharsetProviderICU;
 import com.ibm.icu.charset.CharsetSelector;
 import com.ibm.icu.dev.test.TestFmwk;
+import com.ibm.icu.text.UTF16;
 import com.ibm.icu.text.UnicodeSet;
 
 public class TestSelection extends TestFmwk {
+    @SuppressWarnings("deprecation")
+    private String[] availableCharsetNames = (String[]) CharsetProviderICU.getAvailableNames();
+    
+    private int findIndex(String charsetName) {
+        Charset set1 = Charset.forName(charsetName);
+        for (int i = 0; i < availableCharsetNames.length; i++) {
+            Charset set2 = Charset.forName(availableCharsetNames[i]);
+            if (set1.compareTo(set2) == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    @SuppressWarnings("unchecked")
+    private void fillBool(List result, boolean[] toFill) {
+        for (int i = 0; i < toFill.length; i++) {
+            toFill[i] = false;
+        }
+        for (int i = 0; i < result.size(); i++) {
+            String name = (String) result.get(i);
+            toFill[findIndex(name)] = true;
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    private void verifyResultUTF16(String s, Vector encodings, List result, UnicodeSet excludedEncodings, int mappingTypes) {
+        boolean[] resultsFromSystem = new boolean[availableCharsetNames.length];
+        boolean[] resultsManually = new boolean[availableCharsetNames.length];
+        for (int i = 0; i < availableCharsetNames.length; i++) {
+            resultsFromSystem[i] = resultsManually[i] = false;
+        }
+        
+        int numOfEncodings = encodings.size();
+        for (int i = 0; i < numOfEncodings; i++) {
+            resultsManually[findIndex((String) encodings.get(i))] = true;
+            UnicodeSet unicodePointSet = new UnicodeSet();
+            Charset testCharset = CharsetICU.forNameICU((String) encodings.get(i));
+            ((CharsetICU) testCharset).getUnicodeSet(unicodePointSet, mappingTypes);
+            resultsManually[findIndex((String) encodings.get(i))] = true; // why is it called again? should be removed
+            int ch;
+            int index = 0; 
+            while (index < s.length()) {
+                ch = UTF16.charAt(s, index);
+                if (excludedEncodings.contains(ch) == false && unicodePointSet.contains(ch) == false) {
+                    resultsManually[findIndex((String) encodings.get(i))] = false;
+                    break;
+                } 
+                index += UTF16.getCharCount(ch);
+            }         
+        }
+        
+        // fill the bool for the selector results
+        fillBool(result, resultsFromSystem);
+        for (int i = 0; i < availableCharsetNames.length; i++) {
+            if (resultsManually[i] != resultsFromSystem[i]) {
+                errln("failure in charset selector! Charset " + availableCharsetNames[i] + 
+                      " had conflicting results manual: " + resultsManually[i] + ", system: " + 
+                      resultsFromSystem[i] + "\n");
+            }
+        }
+    }
+    
     public static void main(String[] args) throws Exception {
         new TestSelection().run(args);
     }
     
-    @SuppressWarnings({ "deprecation", "unchecked" })
+    @SuppressWarnings("unchecked")
     public void TestConversionUTF16() {
         /* 
          * test cases are separated by a -1
@@ -51,8 +117,7 @@ public class TestSelection extends TestFmwk {
                 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
                 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189,
                 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, -1, 1, -1};
-        String[] availableCharsetNames = (String[]) CharsetProviderICU.getAvailableNames();
-        String[] texts = {
+        String[] texts = {""
         };
         
         UnicodeSet[] excludedSets = new UnicodeSet[3];
@@ -76,11 +141,13 @@ public class TestSelection extends TestFmwk {
                 }
                 CharsetSelector sel = new CharsetSelector(encodings, excludedSets[excludedSetId], CharsetICU.ROUNDTRIP_SET);
                 
-                
-                
+                List result;
+                for (int i = 0; i < texts.length; i++) {
+                    result = sel.selectForString(texts[i]);
+                    verifyResultUTF16(texts[i], encodings, result, excludedSets[excludedSetId], CharsetICU.ROUNDTRIP_SET);
+                } 
+                prev = testCaseIdx + 1;
             }
         }
-        
     }
-
 }
