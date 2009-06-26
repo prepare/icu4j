38ee194fc4d8f11f070811b9935b0865724a2c26
dougfelt <dougfelt@251d0590-4201-4cf1-90de-194747b24ca1>
2009-06-26 23:03:23 +0000

    ticket:7022: fix common-targets, move docs from RBNF to default scanner

--- icu4j/branches/dougfelt/collate/main/classes/collate/src/com/ibm/icu/text/RbnfScannerProviderImpl.java
+++ icu4j/branches/dougfelt/collate/main/classes/collate/src/com/ibm/icu/text/RbnfScannerProviderImpl.java
@@ -12,6 +12,10 @@ import com.ibm.icu.util.ULocale;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Returns RbnfLenientScanners that use the old RuleBasedNumberFormat
+ * implementation behind setLenientParseMode, which is based on Collator.
+ */
 public class RbnfScannerProviderImpl implements RbnfLenientScannerProvider {
     private Map<String, RbnfLenientScanner> cache;
 
@@ -19,6 +23,34 @@ public class RbnfScannerProviderImpl implements RbnfLenientScannerProvider {
         cache = new HashMap<String, RbnfLenientScanner>();
     }
 
+    /**
+     * Returns a collation-based scanner.
+     *
+     * Only primary differences are treated as significant.  This means that case
+     * differences, accent differences, alternate spellings of the same letter
+     * (e.g., ae and a-umlaut in German), ignorable characters, etc. are ignored in
+     * matching the text.  In many cases, numerals will be accepted in place of words
+     * or phrases as well.
+     *
+     * For example, all of the following will correctly parse as 255 in English in
+     * lenient-parse mode:
+     * <br>"two hundred fifty-five"
+     * <br>"two hundred fifty five"
+     * <br>"TWO HUNDRED FIFTY-FIVE"
+     * <br>"twohundredfiftyfive"
+     * <br>"2 hundred fifty-5"
+     *
+     * The Collator used is determined by the locale that was
+     * passed to this object on construction.  The description passed to this object
+     * on construction may supply additional collation rules that are appended to the
+     * end of the default collator for the locale, enabling additional equivalences
+     * (such as adding more ignorable characters or permitting spelled-out version of
+     * symbols; see the demo program for examples).
+     *
+     * It's important to emphasize that even strict parsing is relatively lenient: it
+     * will accept some text that it won't produce as output.  In English, for example,
+     * it will correctly parse "two hundred zero" and "fifteen hundred".
+     */
     public RbnfLenientScanner get(ULocale locale, String extras) {
         RbnfLenientScanner result = null;
         String key = locale.toString() + "/" + extras;
