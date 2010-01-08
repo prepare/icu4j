cf73c1d65b6b9d0813b49a135bd40529e0f1d5ca
kirtig <kirtig@251d0590-4201-4cf1-90de-194747b24ca1>
2010-01-08 19:39:16 +0000

    ticket:3931: Updated test cases

--- icu4j/branches/kirtig/main/tests/core/src/com/ibm/icu/dev/test/format/SelectFormatTest.java
+++ icu4j/branches/kirtig/main/tests/core/src/com/ibm/icu/dev/test/format/SelectFormatTest.java
@@ -37,8 +37,8 @@ static final int NUM_OF_FORMAT_ARGS = 3 ;
     //Default constructor
     try{
         SelectFormat selFmt = new SelectFormat();
-    }catch(UnsupportedOperationException e){
-       System.out.println("Not implemented yet - TestConstructors while creating a default constructor");
+        assertNotNull("Error: TestConstructors - SelectFormat object constructed "
+                      + "with default constructor is null" , selFmt );  
     }catch(Exception e){
         errln("Exception encountered in TestConstructors while creating a default constructor");
     }
@@ -46,8 +46,8 @@ static final int NUM_OF_FORMAT_ARGS = 3 ;
     //Constructor with argument - a pattern
     try{
         SelectFormat selFmt = new SelectFormat(SIMPLE_PATTERN);
-    }catch(UnsupportedOperationException e){
-       System.out.println("Not implemented yet - TestConstructors while creating a constructor with argument");
+        assertNotNull("Error: TestConstructors - SelectFormat object constructed "
+                      + "with argument constructor is null" , selFmt );  
     }catch(Exception e){
         errln("Exception encountered in TestConstructors while creating a constructor with argument");
     }
@@ -63,91 +63,144 @@ static final int NUM_OF_FORMAT_ARGS = 3 ;
     try{
         selFmt1 = new SelectFormat();
         SelectFormat selFmt2 = new SelectFormat();
-        if( selFmt1.equals(selFmt2)){
-            errln("Equals test failed while checking equality for default constructed SelectFormats ."); 
-        }
-    }catch(UnsupportedOperationException e){
-       System.out.println("Not implemented yet - TestEquals ");
-       //assertEquals("Equals test failed." ,selFmt1 , selFmt2); 
+        assertTrue("Equals test failed while checking equality for " 
+                   + "default constructed SelectFormats ." 
+                   , selFmt1.equals(selFmt2) ); 
     }catch(Exception e){
-        errln("Exception encountered in TestEquals");
+        errln("Exception encountered in TestEquals 1 " + e.getMessage());
     }
 
     //Check equality for pattern constructed SelectFormats
     try{
         selFmt3 = new SelectFormat(SIMPLE_PATTERN);
         SelectFormat selFmt4 = new SelectFormat(SIMPLE_PATTERN);
-        if( selFmt3.equals(selFmt4)){
-            errln("Equals test failed while checking equality for pattern constructed SelectFormats ."); 
-        }
-    }catch(UnsupportedOperationException e){
-       System.out.println("Not implemented yet - TestEquals ");
-       //assertEquals("Equals test failed." ,selFmt1 , selFmt2); 
+        assertTrue("Equals test failed while checking equality for " 
+                   + "pattern constructed SelectFormats ." 
+                   , selFmt3.equals(selFmt4) ); 
     }catch(Exception e){
-        errln("Exception encountered in TestEquals");
+        errln("Exception encountered in TestEquals 2 " + e.getMessage());
     }
 
     //Check equality for 2 objects  
     try{
         Object selFmt5 = new SelectFormat();
         Object selFmt6 = new SelectFormat(SIMPLE_PATTERN);
-        if( selFmt1.equals(selFmt5)){
-            errln("Equals test failed while checking equality for object."); 
-        }
-        if( selFmt3.equals(selFmt6)){
-            errln("Equals test failed while checking equality for object."); 
-        }
-    }catch(UnsupportedOperationException e){
-       System.out.println("Not implemented yet - TestEquals ");
-       //assertEquals("Equals test failed." ,selFmt1 , selFmt3); 
+        assertTrue("Equals test failed while checking equality for object 1." 
+                    , selFmt1.equals(selFmt5) );
+        assertTrue("Equals test failed while checking equality for object 2." 
+                    , selFmt3.equals(selFmt6) );
     }catch(Exception e){
-        errln("Exception encountered in TestEquals");
+        errln("Exception encountered in TestEquals 3" + e.getMessage());
     }
-
   }//end of TestEquals
 
   public void TestApplyPatternToPattern() {
     log("Inside TestApplyPatternToPattern");
     System.out.println("\nInside TestApplyPatternToPattern");
+    SelectFormat selFmt = null;
     String pattern = "masculine{masculineVerbValue} other{otherVerbValue}";
 
+    //Check toString for Default constructed SelectFormat
     try{
-        SelectFormat selFmt1 = new SelectFormat(SIMPLE_PATTERN);
-        selFmt1.applyPattern(pattern);
-        assertEquals("Failed in applyPattern,toPattern with unexpected output",( selFmt1.toPattern()), pattern);
-    }catch(UnsupportedOperationException e){
-       System.out.println("Not implemented yet - TestApplyPatternToPattern ");
+        selFmt = new SelectFormat();
+        selFmt.applyPattern(pattern);
+        assertEquals("Failed in applyPattern,toPattern 1 with unexpected output"
+                     , pattern,  selFmt.toPattern() );
     }catch(Exception e){
-        errln("Exception encountered in TestApplyPatternToPattern");
+        errln("Exception encountered in TestApplyPatternToPattern 1");
+    }
+
+    //Check toString for Default constructed SelectFormat
+    try{
+        selFmt = new SelectFormat(SIMPLE_PATTERN);
+        selFmt.applyPattern(pattern);
+        assertEquals("Failed in applyPattern,toPattern 2 with unexpected output"
+                     , pattern,  selFmt.toPattern() );
+    }catch(Exception e){
+        errln("Exception encountered in TestApplyPatternToPattern 2");
     }
   }
 
   public void TestToString(){
     log("Inside TestToString");
     System.out.println("\nInside TestToString");
+    SelectFormat selFmt = null;
+
+    //Check toString for Default constructed SelectFormat
     try{
-        SelectFormat selFmt1 = new SelectFormat(SIMPLE_PATTERN);
-        String expected = "";
-        selFmt1.toString();
-        assertEquals("Failed in TestToString with unexpected output", (selFmt1.toString()), expected);
-    }catch(UnsupportedOperationException e){
-       System.out.println("Not implemented yet-ToString");
+        selFmt = new SelectFormat();
+        String expected = "pattern='null', parsedValues='null'";
+        assertEquals("Failed in TestToString with unexpected output 1"
+                     , expected, selFmt.toString() );
     }catch(Exception e){
-        errln("Exception encountered in TestToString");
+        errln("Exception encountered in TestToString 1");
+    }
+
+    //Check toString for pattern constructed SelectFormat
+    try{
+        selFmt = new SelectFormat(SIMPLE_PATTERN);
+        String expected = "pattern='feminine {feminineVerbValue} other{otherVerbValue}'"
+                          + ", parsedValues='{other=otherVerbValue, "
+                          + "feminine=feminineVerbValue}'";
+        assertEquals("Failed in TestToString with unexpected output 2"
+                     , expected, selFmt.toString() );
+    }catch(Exception e){
+        errln("Exception encountered in TestToString 2");
     }
   }
 
   public void TestHashCode(){
     log("Inside TestHashCode");
     System.out.println("\nInside TestHashCode");
+    SelectFormat selFmt = null;
+
+    //Check hashCode for Default constructed SelectFormat
     try{
-        SelectFormat selFmt1 = new SelectFormat();
+        selFmt = new SelectFormat();
         int expected = 0;
-        assertEquals("Failed in TestHashCode with unexpected output", (selFmt1.hashCode()), expected);
-    }catch(UnsupportedOperationException e){
-       System.out.println("Not implemented yet-HashCode");
+        assertEquals("Failed in TestHashCode 1 with unexpected output"
+                     , expected, selFmt.hashCode() );
     }catch(Exception e){
-        errln("Exception encountered in TestHashCode");
+        errln("Exception encountered in TestHashCode 1 with message : " 
+              + e.getMessage());
+    }
+
+    //Check hashCode for pattern constructed SelectFormat
+    try{
+        selFmt = new SelectFormat(SIMPLE_PATTERN);
+        int expected = -1199738118;
+        assertEquals("Failed in TestHashCode 2 with unexpected output"
+                     , expected, selFmt.hashCode() );
+    }catch(Exception e){
+        errln("Exception encountered in TestHashCode 2 with message : " 
+              + e.getMessage());
+    }
+
+  }
+
+  public void TestToPattern(){
+    log("Inside TestToPattern");
+    System.out.println("\nInside TestToPattern");
+    SelectFormat selFmt = null;
+
+    //Check toPattern for Default constructed SelectFormat
+    try{
+        selFmt = new SelectFormat();
+        assertEquals("Failed in TestToPattern 1 with unexpected output"
+                     , null, selFmt.toPattern() );
+    }catch(Exception e){
+        errln("Exception encountered in TestToPattern 1 with message : " 
+              + e.getMessage());
+    }
+
+    //Check toPattern for pattern constructed SelectFormat
+    try{
+        selFmt = new SelectFormat(SIMPLE_PATTERN);
+        assertEquals("Failed in TestToPattern 2 with unexpected output"
+                     , SIMPLE_PATTERN, selFmt.toPattern() );
+    }catch(Exception e){
+        errln("Exception encountered in TestToPattern 2 with message : " 
+              + e.getMessage());
     }
   }
 
@@ -155,27 +208,54 @@ static final int NUM_OF_FORMAT_ARGS = 3 ;
     log("Inside TestFormat");
     System.out.println("\nInside TestFormat");
 
+    //Check format for pattern constructed object
     try{
         SelectFormat selFmt1 = new SelectFormat(SIMPLE_PATTERN);
         String expected = "feminineVerbValue";
-        assertEquals("Failed in TestFormat with unexpected output", (selFmt1.format("feminine")), expected);
-    }catch(UnsupportedOperationException e){
-       System.out.println("Not implemented yet-TestFormat");
+        assertEquals("Failed in TestFormat with unexpected output 1"
+                     , expected 
+                     , selFmt1.format("feminine") );
     }catch(Exception e){
-        errln("Exception encountered in TestFormat");
+        errln("Exception encountered in TestFormat 3: " + e.getMessage() );
     }
 
+    //Check format with appendTo for pattern constructed object
     try{
         SelectFormat selFmt1 = new SelectFormat(SIMPLE_PATTERN);
-        String expected = "otherVerbValue";
+        StringBuffer expected = new StringBuffer("AppendHere-otherVerbValue");
         StringBuffer strBuf = new StringBuffer("AppendHere-");
-        assertEquals("Failed in TestFormat with unexpected output 2", (selFmt1.format("other",strBuf, new FieldPosition(0) )), expected);
-    }catch(UnsupportedOperationException e){
-       System.out.println("Not implemented yet-TestFormat");
+        assertEquals("Failed in TestFormat with unexpected output 2"
+                     , expected.toString()
+                     , (selFmt1.format("other",strBuf, new FieldPosition(0))).toString()  );
     }catch(Exception e){
-        errln("Exception encountered in TestFormat");
+        errln("Exception encountered in TestFormat 2: " + e.getMessage() );
     }
 
+    //Check format for Default constructed object
+    SelectFormat selFmt1 = new SelectFormat();
+    try{
+        selFmt1.format("feminine");
+        fail("Failed in TestFormat 4 as did not receive the expected IllegalArgumentException.");
+    }catch(IllegalArgumentException e){
+        String expected = "Invalid format error.";
+        assertEquals("Failed in TestFormat 4 with unexpected excpetion message"
+                    , expected , e.getMessage() );
+    }catch(Exception e){
+        fail("Failed in TestFormat 4 with exception as " + e.getMessage());
+    }
+
+    //Check format with appendTo for Default constructed object
+    try{
+        StringBuffer strBuf = new StringBuffer("AppendHere-");
+        selFmt1.format("other",strBuf, new FieldPosition(0));
+        fail("Failed in TestFormat 4 as did not receive the expected IllegalArgumentException.");
+    }catch(IllegalArgumentException e){
+        String expected = "Invalid format error.";
+        assertEquals("Failed in TestFormat 4 with unexpected excpetion message"
+                    , expected , e.getMessage() );
+    }catch(Exception e){
+        fail("Failed in TestFormat 4 with exception as " + e.getMessage());
+    }
   }
 
 }
