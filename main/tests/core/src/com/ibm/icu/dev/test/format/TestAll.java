9b0fa7c95264ff818c8a90cc2ce9a3f0bbf61f2c
kirtig <kirtig@251d0590-4201-4cf1-90de-194747b24ca1>
2010-01-12 20:15:44 +0000

    ticket:3931: Better name for the file.

--- main/tests/core/src/com/ibm/icu/dev/test/format/TestAll.java
+++ main/tests/core/src/com/ibm/icu/dev/test/format/TestAll.java
@@ -111,7 +111,7 @@ public class TestAll extends TestGroup {
         public SelectFormat() {
             super(new String[] {
                 "SelectFormatUnitTest",
-                "SelectFormatTest",
+                "SelectFormatAPITest",
             });
         }
     }
