ddd87436f0e13c78ef1beef78e0a89bf1e342acd
yoshito <yoshito@251d0590-4201-4cf1-90de-194747b24ca1>
2011-03-31 06:56:31 +0000

    ticket:8342: Generic location format implementation.

--- t8342/main/classes/core/src/com/ibm/icu/impl/TimeZoneFormatImpl.java
+++ t8342/main/classes/core/src/com/ibm/icu/impl/TimeZoneFormatImpl.java
@@ -6,8 +6,11 @@
  */
 package com.ibm.icu.impl;
 
+import java.text.MessageFormat;
 import java.util.Date;
+import java.util.MissingResourceException;
 
+import com.ibm.icu.text.LocaleDisplayNames;
 import com.ibm.icu.text.TimeZoneFormat;
 import com.ibm.icu.text.TimeZoneNames.NameType;
 import com.ibm.icu.util.TimeZone;
@@ -21,10 +24,39 @@ public class TimeZoneFormatImpl extends TimeZoneFormat {
 
     private ULocale _locale;
 
+    private enum Pattern {
+        // The format pattern such as "{0} Time", where {0} is the country. 
+        REGION_FORMAT("regionFormat", "({0})"),
+
+        // The format pattern such as "{1} Time ({0})", where {1} is the country and {0} is a city.
+        FALLBACK_REGION_FORMAT("fallbackRegionFormat", "{1} ({0})"),
+
+        // The format pattern such as "{1} ({0})", where {1} is the metazone, and {0} is the country or city.
+        FALLBACK_FORMAT("fallbackFormat", "({0})");
+
+        String _key;
+        String _defaultVal;
+
+        Pattern(String key, String defaultVal) {
+            _key = key;
+            _defaultVal = defaultVal;
+        }
+
+        String key() {
+            return _key;
+        }
+
+        String defaultValue() {
+            return _defaultVal;
+        }
+    }
+
+    private transient MessageFormat[] _patternFormatters = new MessageFormat[Pattern.values().length];
     private transient boolean _frozen;
 
     public TimeZoneFormatImpl(ULocale locale) {
-        
+        // TODO
+        _locale = locale;
     }
 
     /* (non-Javadoc)
@@ -81,9 +113,21 @@ public class TimeZoneFormatImpl extends TimeZoneFormat {
      */
     @Override
     protected String handleFormatGenericLocation(TimeZone tz) {
-        // TODO format location
-        String name = getTimeZoneNames().getExemplarLocationName(tz.getID());
-        return name;
+        String tzID = tz.getID();
+        String countryCode = ZoneMeta.getCanonicalCountry(tzID);
+        if (countryCode != null) {
+            String country = LocaleDisplayNames.getInstance(_locale).regionDisplayName(countryCode);
+            if (ZoneMeta.getSingleCountry(tzID) != null) {
+                // If the zone is only one zone in the country, do not add city
+                return formatPattern(Pattern.REGION_FORMAT, country);
+            } else {
+                // getExemplarLocationName should return non-empty String
+                // if the time zone is associated with a location
+                String city = getTimeZoneNames().getExemplarLocationName(tzID);
+                return formatPattern(Pattern.FALLBACK_REGION_FORMAT, city, country);
+            }
+        }
+        return null;
     }
 
     /* (non-Javadoc)
@@ -129,6 +173,23 @@ public class TimeZoneFormatImpl extends TimeZoneFormat {
     protected ParseResult handleParseGenericLocation(String text, int start) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    private synchronized String formatPattern(TimeZoneFormatImpl.Pattern pat, String... args) {
+        int idx = pat.ordinal();
+        if (_patternFormatters[idx] == null) {
+            String patText;
+            try {
+                ICUResourceBundle bundle = (ICUResourceBundle) ICUResourceBundle.getBundleInstance(
+                    ICUResourceBundle.ICU_ZONE_BASE_NAME, _locale);
+                patText = bundle.getStringWithFallback("zoneStrings/" + pat.key());
+            } catch (MissingResourceException e) {
+                patText = pat.defaultValue();
+            }
+
+            _patternFormatters[idx] = new MessageFormat(patText);
+        }
+        return _patternFormatters[idx].format(args);
     }
 
     /* (non-Javadoc)
