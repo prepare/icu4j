51bfdc791b417ff098714778cc965923ac5c6138
tkeep <tkeep@251d0590-4201-4cf1-90de-194747b24ca1>
2012-08-17 16:47:44 +0000

    ticket:9480: First draft of new ICULoadingCache interface

---
+++ cache/main/classes/core/src/com/ibm/icu/impl/ICULoadingCache.java
@@ -0,0 +1,41 @@
+/*
+ *******************************************************************************
+ * Copyright (C) 2012, International Business Machines Corporation and         *
+ * others. All Rights Reserved.                                                *
+ *******************************************************************************
+ */
+package com.ibm.icu.impl;
+
+/**
+ * Interface for very simple caching.
+ * 
+ * @param <K> The key type of cache
+ * @param <V> the value type of cache
+ * @author Travis Keep
+ */
+public interface ICULoadingCache<K, V> {
+    /**
+     * Invalidates all entries in this cache. This causes any subsequent calls
+     * to {@link #get} to load the new value.
+     */
+    void invalidateAll();
+    
+    /**
+     * Gets value from cache. If no value for key is present in cache, the cache
+     * loads the value, stores it in the cache, and returns that value. If
+     * another thread is already loading the value for key then the calling
+     * thread blocks until the value is available.
+     * 
+     * @param key the cache key
+     * @return the non-null cache value
+     */
+    V get(K key);
+    
+    /**
+     * Like {@link #get}, but if no value for key is present in the cache,
+     * immediately returns null.
+     * @param key the cache key
+     * @return the cache value or null if no value for key stored in cache.
+     */
+    V getIfPresent(Object key);
+}
