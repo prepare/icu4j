27b5256dc4625abaa948fa3ee985e58cbebcb1a9
yoshito <yoshito@251d0590-4201-4cf1-90de-194747b24ca1>
2012-11-16 22:06:18 +0000

    ticket:9750: Tagged maint/eclipse-icu50 r32857 as release-50-1-eclipse-20121116.

--- main/classes/core/src/com/ibm/icu/text/ListFormatter.java
+++ main/classes/core/src/com/ibm/icu/text/ListFormatter.java
@@ -178,12 +178,11 @@ final public class ListFormatter {
         private static ListFormatter load(ULocale ulocale) {
             ICUResourceBundle r = (ICUResourceBundle)UResourceBundle.
                     getBundleInstance(ICUResourceBundle.ICU_BASE_NAME, ulocale);
-            r = r.getWithFallback("listPattern/standard");
             return new ListFormatter(
-                r.getWithFallback("2").getString(),
-                r.getWithFallback("start").getString(),
-                r.getWithFallback("middle").getString(),
-                r.getWithFallback("end").getString());
+                r.getWithFallback("listPattern/standard/2").getString(),
+                r.getWithFallback("listPattern/standard/start").getString(),
+                r.getWithFallback("listPattern/standard/middle").getString(),
+                r.getWithFallback("listPattern/standard/end").getString());
         }
     }
 
