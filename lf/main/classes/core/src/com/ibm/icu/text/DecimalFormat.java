d4020ec1415ab955d918d357e73853175fe67618
tkeep <tkeep@251d0590-4201-4cf1-90de-194747b24ca1>
2012-11-16 00:50:15 +0000

    ticket:9691: Merge r32809 to r32845 from trunk

--- lf/main/classes/core/src/com/ibm/icu/text/DecimalFormat.java
+++ lf/main/classes/core/src/com/ibm/icu/text/DecimalFormat.java
@@ -1,4 +1,3 @@
-//##header
 /*
  *******************************************************************************
  * Copyright (C) 1996-2012, International Business Machines Corporation and    *
@@ -348,7 +347,7 @@ import com.ibm.icu.util.ULocale.Category;
  * <code>DecimalFormat</code> internally limits of maximum decimal digits to be 1000. Thus,
  * an input string resulting more than 1000 digits in plain decimal representation (non-exponent)
  * will be treated as either overflow (positive/negative infinite) or underflow (+0.0/-0.0).
- * 
+ *
  * <h4>Formatting</h4>
  *
  * <p>Formatting is guided by several parameters, all of which can be specified either
@@ -3068,17 +3067,11 @@ public class DecimalFormat extends NumberFormat {
      * @see #setRoundingMode
      * @stable ICU 2.0
      */
-//#if defined(ECLIPSE)
-//##    public BigDecimal getRoundingIncrement() {
-//##        return roundingIncrementICU;
-//##    }
-//#else
     public java.math.BigDecimal getRoundingIncrement() {
         if (roundingIncrementICU == null)
             return null;
         return roundingIncrementICU.toBigDecimal();
     }
-//#endif
 
     /**
      * {@icu} Sets the rounding increment. This method also controls whether rounding is
@@ -5703,6 +5696,18 @@ public class DecimalFormat extends NumberFormat {
 
         public void writePrefix(StringBuffer toAppendTo) {
             toAppendTo.append(prefix);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (!(obj instanceof Unit)) {
+                return false;
+            }
+            Unit other = (Unit) obj;
+            return prefix.equals(other.prefix) && suffix.equals(other.suffix);
         }
     }
 
