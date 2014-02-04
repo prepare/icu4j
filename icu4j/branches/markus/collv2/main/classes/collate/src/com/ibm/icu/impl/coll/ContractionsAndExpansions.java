7054420f76f5d94f49238c78b7a0a152a2b5f3dd
yoshito <yoshito@251d0590-4201-4cf1-90de-194747b24ca1>
2014-02-04 23:50:52 +0000

    ticket:9101: Port ContractionsAndExpansions to Java

--- icu4j/branches/markus/collv2/main/classes/collate/src/com/ibm/icu/impl/coll/ContractionsAndExpansions.java
+++ icu4j/branches/markus/collv2/main/classes/collate/src/com/ibm/icu/impl/coll/ContractionsAndExpansions.java
@@ -11,191 +11,168 @@
 
 package com.ibm.icu.impl.coll;
 
+import java.util.Iterator;
+
+import com.ibm.icu.impl.Trie2;
+import com.ibm.icu.text.UnicodeSet;
+import com.ibm.icu.util.CharsTrie;
+import com.ibm.icu.util.CharsTrie.Entry;
+
 final class ContractionsAndExpansions {
-public:
-    interface CESink {
-        void handleCE(long ce);
-        void handleExpansion(const long ces[], int length);
-    };
 
-    ContractionsAndExpansions(UnicodeSet *con, UnicodeSet *exp, CESink *s, boolean prefixes)
-            : data(null), tailoring(null),
-              contractions(con), expansions(exp),
-              sink(s),
-              addPrefixes(prefixes),
-              checkTailored(0),
-              suffix(null),
-              errorCode(U_ZERO_ERROR) {}
-
-    void forData(CollationData d);
-    void forCodePoint(CollationData d, int c, UErrorCode &ec);
-
-    // all following: @internal, only public for access by callback
-
-    void handleCE32(int start, int end, int ce32);
-
-    void handlePrefixes(int start, int end, int ce32);
-    void handleContractions(int start, int end, int ce32);
-
-    void addExpansions(int start, int end);
-    void addStrings(int start, int end, UnicodeSet *set);
-
-    /** Prefixes are reversed in the data structure. */
-    void setPrefix(const UnicodeString &pfx) {
-        unreversedPrefix = pfx;
-        unreversedPrefix.reverse();
-    }
-    void resetPrefix() {
-        unreversedPrefix.remove();
-    }
-
+    // TODO: Do we really want to make the scope of these fields 'package' instead of 'private'?
+    //      CollationData tailoring is set, but not referenced by the implementation in this class.
     CollationData data;
     CollationData tailoring;
-    UnicodeSet *contractions;
-    UnicodeSet *expansions;
-    CESink *sink;
+    UnicodeSet contractions;
+    UnicodeSet expansions;
+    CESink sink;
     boolean addPrefixes;
-    int8_t checkTailored;  // -1: collected tailored  +1: exclude tailored
+    int checkTailored = 0;  // -1: collected tailored  +1: exclude tailored
     UnicodeSet tailored;
     UnicodeSet ranges;
-    UnicodeString unreversedPrefix;
-    const UnicodeString *suffix;
-    long ces[Collation.MAX_EXPANSION_LENGTH];
-    UErrorCode errorCode;
-}
+    StringBuilder unreversedPrefix = new StringBuilder();
+    String suffix;
+    long[] ces = new long[Collation.MAX_EXPANSION_LENGTH];
 
-    ContractionsAndExpansions.CESink.~CESink() {}
-
-    static boolean U_CALLCONV
-    enumCnERange(const void *context, int start, int end, int ce32) {
-        ContractionsAndExpansions *cne = (ContractionsAndExpansions *)context;
-        if(cne.checkTailored == 0) {
-            // There is no tailoring.
-            // No need to collect nor check the tailored set.
-        } else if(cne.checkTailored < 0) {
-            // Collect the set of code points with mappings in the tailoring data.
-            if(ce32 == Collation.FALLBACK_CE32) {
-                return true;  // fallback to base, not tailored
-            } else {
-                cne.tailored.add(start, end);
-            }
-            // checkTailored > 0: Exclude tailored ranges from the base data enumeration.
-        } else if(start == end) {
-            if(cne.tailored.contains(start)) {
-                return true;
-            }
-        } else if(cne.tailored.containsSome(start, end)) {
-            cne.ranges.set(start, end).removeAll(cne.tailored);
-            int count = cne.ranges.getRangeCount();
-            for(int i = 0; i < count; ++i) {
-                cne.handleCE32(cne.ranges.getRangeStart(i), cne.ranges.getRangeEnd(i), ce32);
-            }
-            return U_SUCCESS(cne.errorCode);
-        }
-        cne.handleCE32(start, end, ce32);
-        return U_SUCCESS(cne.errorCode);
+    public static interface CESink {
+        void handleCE(long ce);
+        void handleExpansion(long ces[], int start, int length);
     }
 
-    void
-    ContractionsAndExpansions.forData(CollationData d, UErrorCode &ec) {
-        if(U_FAILURE(ec)) { return; }
-        errorCode = ec;  // Preserve info & warning codes.
+    public ContractionsAndExpansions(UnicodeSet con, UnicodeSet exp, CESink s, boolean prefixes) {
+        contractions = con;
+        expansions = exp;
+        sink = s;
+        addPrefixes = prefixes;
+    }
+
+    public void forData(CollationData d) {
         // Add all from the data, can be tailoring or base.
-        if(d.base != null) {
+        if (d.base != null) {
             checkTailored = -1;
         }
         data = d;
-        utrie2_enum(data.trie, null, enumCnERange, this);
-        if(d.base == null || U_FAILURE) {
-            ec = errorCode;
-            return;
+        Iterator<Trie2.Range> trieIterator = data.trie.iterator();
+        Trie2.Range range;
+        while (trieIterator.hasNext() && !(range = trieIterator.next()).leadSurrogate) {
+            enumCnERange(range.startCodePoint, range.endCodePoint, range.value, this);
         }
+        // Java porting note: handleCE32() throws RuntimeException when it encounters unexpected
+        // a CE32 type. Otherwise, d.base should not be null.
+        assert (d.base != null);
+
         // Add all from the base data but only for un-tailored code points.
         tailored.freeze();
         checkTailored = 1;
         tailoring = d;
         data = d.base;
-        utrie2_enum(data.trie, null, enumCnERange, this);
-        ec = errorCode;
+        trieIterator = data.trie.iterator();
+        while (trieIterator.hasNext() && !(range = trieIterator.next()).leadSurrogate) {
+            enumCnERange(range.startCodePoint, range.endCodePoint, range.value, this);
+        }
     }
 
-    void
-    ContractionsAndExpansions.forCodePoint(CollationData d, int c, UErrorCode &ec) {
-        if(U_FAILURE(ec)) { return; }
-        errorCode = ec;  // Preserve info & warning codes.
+    private void enumCnERange(int start, int end, int ce32, ContractionsAndExpansions cne) {
+        if (cne.checkTailored == 0) {
+            // There is no tailoring.
+            // No need to collect nor check the tailored set.
+        } else if (cne.checkTailored < 0) {
+            // Collect the set of code points with mappings in the tailoring data.
+            if (ce32 == Collation.FALLBACK_CE32) {
+                return; // fallback to base, not tailored
+            } else {
+                cne.tailored.add(start, end);
+            }
+            // checkTailored > 0: Exclude tailored ranges from the base data enumeration.
+        } else if (start == end) {
+            if (cne.tailored.contains(start)) {
+                return;
+            }
+        } else if (cne.tailored.containsSome(start, end)) {
+            cne.ranges.set(start, end).removeAll(cne.tailored);
+            int count = cne.ranges.getRangeCount();
+            for (int i = 0; i < count; ++i) {
+                cne.handleCE32(cne.ranges.getRangeStart(i), cne.ranges.getRangeEnd(i), ce32);
+            }
+        }
+        cne.handleCE32(start, end, ce32);
+    }
+
+    public void forCodePoint(CollationData d, int c) {
         int ce32 = d.getCE32(c);
-        if(ce32 == Collation.FALLBACK_CE32) {
+        if (ce32 == Collation.FALLBACK_CE32) {
             d = d.base;
             ce32 = d.getCE32(c);
         }
         data = d;
         handleCE32(c, c, ce32);
-        ec = errorCode;
     }
 
-    void
-    ContractionsAndExpansions.handleCE32(int start, int end, int ce32) {
-        for(;;) {
-            if((ce32 & 0xff) < Collation.SPECIAL_CE32_LOW_BYTE) {
+    private void handleCE32(int start, int end, int ce32) {
+        for (;;) {
+            if ((ce32 & 0xff) < Collation.SPECIAL_CE32_LOW_BYTE) {
                 // !isSpecialCE32()
-                if(sink != null) {
+                if (sink != null) {
                     sink.handleCE(Collation.ceFromSimpleCE32(ce32));
                 }
                 return;
             }
-            switch(Collation.tagFromCE32(ce32)) {
+            switch (Collation.tagFromCE32(ce32)) {
             case Collation.FALLBACK_TAG:
                 return;
             case Collation.RESERVED_TAG_3:
             case Collation.BUILDER_DATA_TAG:
             case Collation.LEAD_SURROGATE_TAG:
-                if(U_SUCCESS) { errorCode = U_INTERNAL_PROGRAM_ERROR; }
-                return;
+                // Java porting note: U_INTERNAL_PROGRAM_ERROR is set to errorCode in ICU4C.
+                throw new RuntimeException("Unexpected CE32 tag type ("
+                        + Collation.tagFromCE32(ce32) + ") for CE32 value: " + ce32);
             case Collation.LONG_PRIMARY_TAG:
-                if(sink != null) {
+                if (sink != null) {
                     sink.handleCE(Collation.ceFromLongPrimaryCE32(ce32));
                 }
                 return;
             case Collation.LONG_SECONDARY_TAG:
-                if(sink != null) {
+                if (sink != null) {
                     sink.handleCE(Collation.ceFromLongSecondaryCE32(ce32));
                 }
                 return;
             case Collation.LATIN_EXPANSION_TAG:
-                if(sink != null) {
+                if (sink != null) {
                     ces[0] = Collation.latinCE0FromCE32(ce32);
                     ces[1] = Collation.latinCE1FromCE32(ce32);
-                    sink.handleExpansion(ces, 2);
+                    sink.handleExpansion(ces, 0, 2);
                 }
                 // Optimization: If we have a prefix,
                 // then the relevant strings have been added already.
-                if(unreversedPrefix.isEmpty()) {
+                if (unreversedPrefix.length() == 0) {
                     addExpansions(start, end);
                 }
                 return;
             case Collation.EXPANSION32_TAG:
-                if(sink != null) {
-                    const uint32_t *ce32s = data.ce32s + Collation.indexFromCE32(ce32);
+                if (sink != null) {
+                    int idx = Collation.indexFromCE32(ce32);
                     int length = Collation.lengthFromCE32(ce32);
-                    for(int i = 0; i < length; ++i) {
-                        ces[i] = Collation.ceFromCE32(*ce32s++);
+                    for (int i = 0; i < length; ++i) {
+                        ces[i] = Collation.ceFromCE32(data.ce32s[idx + i]);
                     }
-                    sink.handleExpansion(ces, length);
+                    sink.handleExpansion(ces, 0, length);
                 }
                 // Optimization: If we have a prefix,
                 // then the relevant strings have been added already.
-                if(unreversedPrefix.isEmpty()) {
+                if (unreversedPrefix.length() == 0) {
                     addExpansions(start, end);
                 }
                 return;
             case Collation.EXPANSION_TAG:
-                if(sink != null) {
+                if (sink != null) {
+                    int idx = Collation.indexFromCE32(ce32);
                     int length = Collation.lengthFromCE32(ce32);
-                    sink.handleExpansion(data.ces + Collation.indexFromCE32(ce32), length);
+                    sink.handleExpansion(data.ces, idx, length);
                 }
                 // Optimization: If we have a prefix,
                 // then the relevant strings have been added already.
-                if(unreversedPrefix.isEmpty()) {
+                if (unreversedPrefix.length() == 0) {
                     addExpansions(start, end);
                 }
                 return;
@@ -210,29 +187,29 @@ public:
                 ce32 = data.ce32s[Collation.indexFromCE32(ce32)];
                 break;
             case Collation.U0000_TAG:
-                assert(start == 0 && end == 0);
+                assert (start == 0 && end == 0);
                 // Fetch the normal ce32 for U+0000 and continue.
                 ce32 = data.ce32s[0];
                 break;
             case Collation.HANGUL_TAG:
-                if(sink != null) {
+                if (sink != null) {
                     // TODO: This should be optimized,
                     // especially if [start..end] is the complete Hangul range. (assert that)
-                    UTF16CollationIterator iter(data, false, null, null, null);
-                    UChar hangul[1] = { 0 };
-                    for(int c = start; c <= end; ++c) {
-                        hangul[0] = (UChar)c;
-                        iter.setText(hangul, hangul + 1);
-                        int length = iter.fetchCEs;
-                        if(U_FAILURE) { return; }
+                    UTF16CollationIterator iter = new UTF16CollationIterator(data);
+                    StringBuilder hangul = new StringBuilder(1);
+                    for (int c = start; c <= end; ++c) {
+                        hangul.setLength(0);
+                        hangul.appendCodePoint(c);
+                        iter.setText(false, hangul, 0);
+                        int length = iter.fetchCEs();
                         // Ignore the terminating non-CE.
-                        assert(length >= 2 && iter.getCE(length - 1) == Collation.NO_CE);
-                        sink.handleExpansion(iter.getCEs(), length - 1);
+                        assert (length >= 2 && iter.getCE(length - 1) == Collation.NO_CE);
+                        sink.handleExpansion(iter.getCEs(), 0, length - 1);
                     }
                 }
                 // Optimization: If we have a prefix,
                 // then the relevant strings have been added already.
-                if(unreversedPrefix.isEmpty()) {
+                if (unreversedPrefix.length() == 0) {
                     addExpansions(start, end);
                 }
                 return;
@@ -246,55 +223,54 @@ public:
         }
     }
 
-    void
-    ContractionsAndExpansions.handlePrefixes(
-            int start, int end, int ce32) {
+    private void handlePrefixes(int start, int end, int ce32) {
         int index = Collation.indexFromCE32(ce32);
-        ce32 = data.getCE32FromContexts(index);  // Default if no prefix match.
+        ce32 = data.getCE32FromContexts(index); // Default if no prefix match.
         handleCE32(start, end, ce32);
-        if(!addPrefixes) { return; }
-        UCharsTrie.Iterator prefixes(data.contexts, index + 2, 0);
-        while(prefixes.next) {
-            setPrefix(prefixes.getString());
+        if (!addPrefixes) {
+            return;
+        }
+        CharsTrie.Iterator prefixes = new CharsTrie(data.contexts, index + 2).iterator();
+        while (prefixes.hasNext()) {
+            Entry e = prefixes.next();
+            setPrefix(e.chars);
             // Prefix/pre-context mappings are special kinds of contractions
             // that always yield expansions.
             addStrings(start, end, contractions);
             addStrings(start, end, expansions);
-            handleCE32(start, end, prefixes.getValue());
+            handleCE32(start, end, e.value);
         }
         resetPrefix();
     }
 
-    void
-    ContractionsAndExpansions.handleContractions(
-            int start, int end, int ce32) {
+    void handleContractions(int start, int end, int ce32) {
         int index = Collation.indexFromCE32(ce32);
-        if((ce32 & Collation.CONTRACT_SINGLE_CP_NO_MATCH) != 0) {
+        if ((ce32 & Collation.CONTRACT_SINGLE_CP_NO_MATCH) != 0) {
             // No match on the single code point.
             // We are underneath a prefix, and the default mapping is just
             // a fallback to the mappings for a shorter prefix.
-            assert(!unreversedPrefix.isEmpty());
+            assert (unreversedPrefix.length() != 0);
         } else {
-            ce32 = data.getCE32FromContexts(index);  // Default if no suffix match.
-            assert(!Collation.isContractionCE32(ce32));
+            ce32 = data.getCE32FromContexts(index); // Default if no suffix match.
+            assert (!Collation.isContractionCE32(ce32));
             handleCE32(start, end, ce32);
         }
-        UCharsTrie.Iterator suffixes(data.contexts, index + 2, 0);
-        while(suffixes.next) {
-            suffix = &suffixes.getString();
+        CharsTrie.Iterator suffixes = new CharsTrie(data.contexts, index + 2).iterator();
+        while (suffixes.hasNext()) {
+            Entry e = suffixes.next();
+            suffix = e.chars.toString();
             addStrings(start, end, contractions);
-            if(!unreversedPrefix.isEmpty()) {
+            if (unreversedPrefix.length() != 0) {
                 addStrings(start, end, expansions);
             }
-            handleCE32(start, end, suffixes.getValue());
+            handleCE32(start, end, e.value);
         }
         suffix = null;
     }
 
-    void
-    ContractionsAndExpansions.addExpansions(int start, int end) {
-        if(unreversedPrefix.isEmpty() && suffix == null) {
-            if(expansions != null) {
+    void addExpansions(int start, int end) {
+        if (unreversedPrefix.length() == 0 && suffix == null) {
+            if (expansions != null) {
                 expansions.add(start, end);
             }
         } else {
@@ -302,16 +278,28 @@ public:
         }
     }
 
-    void
-    ContractionsAndExpansions.addStrings(int start, int end, UnicodeSet *set) {
-        if(set == null) { return; }
-        UnicodeString s(unreversedPrefix);
+    void addStrings(int start, int end, UnicodeSet set) {
+        if (set == null) {
+            return;
+        }
+        StringBuilder s = new StringBuilder(unreversedPrefix);
         do {
-            s.append(start);
-            if(suffix != null) {
-                s.append(*suffix);
+            s.appendCodePoint(start);
+            if (suffix != null) {
+                s.append(suffix);
             }
             set.add(s);
-            s.truncate(unreversedPrefix.length());
-        } while(++start <= end);
+            s.setLength(unreversedPrefix.length());
+        } while (++start <= end);
     }
+
+    // Prefixes are reversed in the data structure.
+    private void setPrefix(CharSequence pfx) {
+        unreversedPrefix.setLength(0);
+        unreversedPrefix.append(pfx).reverse();
+    }
+
+    private void resetPrefix() {
+        unreversedPrefix.setLength(0);
+    }
+}
