cefa1b60c0f82bee3932e673e7613630d7a5b5a5
tkeep <tkeep@251d0590-4201-4cf1-90de-194747b24ca1>
2015-02-07 00:05:48 +0000

    ticket:10458: Add ValueObject proof of concept.

---
+++ 10458_valueobject/main/classes/core/src/com/ibm/icu/impl/ScientificPrecision.java
@@ -0,0 +1,30 @@
+/*
+ *******************************************************************************
+ * Copyright (C) 2015, International Business Machines Corporation and
+ * others. All Rights Reserved.
+ *******************************************************************************
+ */
+package com.ibm.icu.impl;
+
+
+public class ScientificPrecision extends ValueObject<ScientificPrecision> {
+    public static final ScientificPrecision DEFAULT = new ScientificPrecision().freeze();
+    
+    private FixedPrecision mantissa = FixedPrecision.DEFAULT;
+    
+    public FixedPrecision getMantissa() { return mantissa; }
+    public FixedPrecision getMutableMantissa() {
+        mantissa = safeThaw(mantissa);
+        return mantissa;
+    }
+    public void setMantissa(FixedPrecision m) {
+        this.mantissa = safeSet(m);
+    }
+    
+    public ScientificPrecision clone() {
+        ScientificPrecision result = super.clone();
+        result.mantissa.freeze();
+        return result;
+    }
+
+}
